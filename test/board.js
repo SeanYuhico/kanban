6bc07cd153f40288c05c8c482d51df4126a66b29
shasha.v <patriciavillaroman@gmail.com>
2019-08-22 19:29:26 +0800

    updated board, added edit ajax

--- test/board.js
+++ test/board.js
@@ -1,7 +1,3 @@
-
-
-
-
 $( document ).ready(function() {
     $('.add-lane-form').toggle();
     $(".edit").click(function() {
@@ -24,15 +20,59 @@ $( document ).ready(function() {
         $(".edit").show();
     });
     
-    
+    $(document).on( "click", function( event ) {
+        if (event.target.matches(".add-lane-toggle")) {
+            $('.add-lane-form').toggle();
+        } else if (!event.target.matches(".add-lane-form") && $('.add-lane-form').is(":visible") === true) {
+            $('.add-lane-form').toggle();
+        } 
+    });
+
+    $("button#save-edit-card").click(function(){
+
+
+        let id = $(this).attr("data-id"),
+            name = $("#edit-card-name").text(),
+            desc = $("#edit-card-desc").text(),
+            img = $("#edit-img").val();
+
+        console.log(id);
+        console.log(name);
+        console.log(desc);
+        console.log(img);
+        
+        $.ajax({
+            url: "../board/" + id,
+            method: "PUT",
+            data:{
+                id: id,
+                cardname: name,
+                carddesc: desc,
+                filename: img
+            }, 
+            success: function(result){
+                console.log(result);
+                
+                //place db shit here
+                
+            }
+        });
+        /*
+         $.ajax({
+                type:'PUT',
+                url: '/events/update/'+$(this).data('id'),
+                data: {event_name: change}
+            }).done(function(response){
+                console.log(response);
+                window.location.replace('http://localhost:3030/');
+            }).fail(function(response){
+                console.log("Oops not working");
+            });
+            
+
+            
+    });
 });
-$(document).on( "click", function( event ) {
-    console.log("1");
-    if (event.target.matches(".add-lane-toggle")) {
-        console.log("1.2");
-        $('.add-lane-form').toggle();
-    } else if (!event.target.matches(".add-lane-form") && $('.add-lane-form').is(":visible") === true) {
-        console.log("1.1");
-        $('.add-lane-form').toggle();
-    } 
+        */
+    });
 });
